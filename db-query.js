7f4437fd2015b2e99021284397ae3c6c10ea3000
Ev1lSoda <killer.be.29@gmail.com>
2021-05-12 15:13:08 +0300

    a&p

--- db-query.js
+++ db-query.js
@@ -11,10 +11,37 @@ const pool = mysql.createPool({
 
 const queryList = {
   dbinfo: 'show databases',
-  students: `SELECT lu_name, ur_role 
-  FROM students`,
-  professors: `SELECT lu_name, ur_role 
-  FROM professors`,
+  students: `SELECT lu_name
+              FROM students`,
+  addStudent: `INSERT INTO lib_users(lu_name, lu_role_id) VALUES(?, 1)`,
+  professors: `SELECT lu_name
+                FROM professors`,
+  addWorker: `INSERT INTO lib_users(lu_name, lu_role_id) VALUES(?, 3)`,
+  workers: `SELECT lu_name
+            FROM lib_users
+            WHERE lu_role_id=3`,
+  addProfessor: `INSERT INTO lib_users(lu_name, lu_role_id) VALUES(?, 2)`,
+  delUser: `DELETE FROM lib_users
+                WHERE lu_name = ?`,
+  updUser: `UPDATE lib_users
+                SET lu_name = ?
+                WHERE lu_name=?`,
+  bookAuthors: `SELECT ba_name
+             FROM book_authors`,
+  addBookAuthor: `INSERT INTO book_authors(ba_name) VALUES(?)`,
+  delBookAuthor: `DELETE FROM book_authors
+                WHERE ba_name = ?`,
+  updBookAuthor: `UPDATE book_authors
+                SET ba_name = ?
+                WHERE ba_name =?`,
+  bookPublishers: `SELECT bp_name
+             FROM book_publishers`,
+  addBookPublisher: `INSERT INTO book_publishers(bp_name) VALUES(?)`,
+  delBookPublisher: `DELETE FROM book_publishers
+                WHERE bp_name = ?`,
+  updBookPublisher: `UPDATE book_publishers
+                SET bp_name = ?
+                WHERE bp_name =?`,
 };
 
 export default class DbQuery {
@@ -30,3 +57,6 @@ export default class DbQuery {
     });
   }
 }
+// 0	17	20:03:53	DELETE FROM lib_users
+//  WHERE lu_name = 'Vasya'	Error Code: 1175. You are using safe update mode and you tried to update a table without a WHERE that uses a KEY column
+//  To disable safe mode, toggle the option in Preferences -> SQL Queries and reconnect.	0.000 sec
--- index.js
+++ index.js
@@ -18,9 +18,105 @@ app.get('/api/students', (req, res) => {
     .then((rez) => res.status(200).json({ students: rez }))
     .catch((e) => res.status(200).json(e));
 });
+app.post('/api/addStudent', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: req.body.name })
+    .then((rez) => res.status(200).json({ students: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+
+app.post('/api/delUser', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: req.body.name })
+    .then((rez) => res.status(200).json({ deleted: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/updUser', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: [req.body.name, req.body.oldName] })
+    .then((rez) => res.status(200).json({ updated: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+
 app.get('/api/professors', (req, res) => {
   DbQuery.dbUse({ query: getQuery(req.url), data: null })
     .then((rez) => res.status(200).json({ professors: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/addProfessor', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: req.body.name })
+    .then((rez) => res.status(200).json({ Professor: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+
+app.get('/api/workers', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: null })
+    .then((rez) => res.status(200).json({ Workers: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/addWorker', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: req.body.name })
+    .then((rez) => res.status(200).json({ Worker: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+
+app.get('/api/bookAuthors', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: null })
+    .then((rez) => res.status(200).json({ bookAuthors: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/addBookAuthor', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: req.body.name })
+    .then((rez) => res.status(200).json({ created: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/delBookAuthor', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: req.body.name })
+    .then((rez) => res.status(200).json({ deleted: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/updBookAuthor', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: [req.body.name, req.body.oldName] })
+    .then((rez) => res.status(200).json({ updated: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+
+app.get('/api/bookAuthors', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: null })
+    .then((rez) => res.status(200).json({ bookAuthors: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/addBookAuthor', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: req.body.name })
+    .then((rez) => res.status(200).json({ created: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/delBookAuthor', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: req.body.name })
+    .then((rez) => res.status(200).json({ deleted: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/updBookAuthor', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: [req.body.name, req.body.oldName] })
+    .then((rez) => res.status(200).json({ updated: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+
+app.get('/api/bookPublishers', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: null })
+    .then((rez) => res.status(200).json({ bookPublishers: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/addBookPublisher', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: req.body.name })
+    .then((rez) => res.status(200).json({ created: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/delBookPublisher', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: req.body.name })
+    .then((rez) => res.status(200).json({ deleted: rez }))
+    .catch((e) => res.status(200).json(e));
+});
+app.post('/api/updBookPublisher', (req, res) => {
+  DbQuery.dbUse({ query: getQuery(req.url), data: [req.body.name, req.body.oldName] })
+    .then((rez) => res.status(200).json({ updated: rez }))
     .catch((e) => res.status(200).json(e));
 });
 
